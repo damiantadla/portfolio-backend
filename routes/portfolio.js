6b5c966963c097f5da95b834a71eef9b5ce71130
damiantadla <157707788+damiantadla@users.noreply.github.com>
2024-03-11 22:38:43 +0100

    Edit endpoint and imports

--- routes/portfolio.js
+++ routes/portfolio.js
@@ -1,10 +1,50 @@
 const express = require("express");
 const router = express.Router();
-const PortfolioController = require("../controllers/ProjectController");
+const PortfolioController = require("../controllers/PortfolioController");
 const AuthMiddleware = require("../middleware/authMiddleware");
+const multer = require("multer");
+const path = require("path");
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "uploads/portfolio/");
+  },
+  filename: function (req, file, cb) {
+    const extArray = file.mimetype.split("/");
+    const ext = extArray[extArray.length - 1];
+    cb(null, Date.now().toString() + "." + ext);
+  },
+});
+
+const fileFilter = function (req, file, cb) {
+  // Accepted file extensions
+  const allowedExtensions = [
+    ".png",
+    ".jpg",
+    ".jpeg",
+    ".webp",
+    ".gif",
+    ".webm",
+    ".svg",
+  ];
+
+  const ext = path.extname(file.originalname).toLowerCase();
+  if (allowedExtensions.includes(ext)) {
+    cb(null, true);
+  } else {
+    cb(null, false);
+  }
+};
+
+const upload = multer({ storage: storage, fileFilter });
 
 //Create new portfolio (access admin)
-router.post("/new", AuthMiddleware.adminAuth, PortfolioController.newProject);
+router.post(
+  "/new",
+  AuthMiddleware.adminAuth,
+  upload.fields([{ name: "image", maxCount: 1 }]),
+  PortfolioController.newProject,
+);
 
 //Delete item portfolio (access admin)
 router.delete(
@@ -17,6 +57,7 @@ router.delete(
 router.put(
   "/edit/:id",
   AuthMiddleware.adminAuth,
+  upload.fields([{ name: "image", maxCount: 1 }]),
   PortfolioController.updateProject,
 );
 
